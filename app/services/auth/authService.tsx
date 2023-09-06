475879d2395e89b748edd12e8fdee02928cc23c0
eduslug <eduslug93@gmail.com>
2023-09-06 16:40:07 -0300

    .env alteration

--- app/services/auth/authService.tsx
+++ app/services/auth/authService.tsx
@@ -2,7 +2,7 @@
 
 export const authService = {
     async login(prop: { username: string, password: string }): Promise<void> {
-        return fetch(`${process.env.NEXT_PUBLIC_URL_BACK_AND_LOCAL}`, {
+        return fetch(` http://localhost:4000/api/login `, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -12,12 +12,12 @@ export const authService = {
                 password: prop.password,
             })
         })
-        /*  */    .then(async (respose) => {
-                if (!respose.ok) throw new Error('Usuario invalido')
-                console.log(respose)
-                const body = await respose.json()
-                console.log(body);
-            })
+        /*  */.then(async (respose) => {
+            if (!respose.ok) throw new Error('Usuario invalido')
+            console.log(respose)
+            const body = await respose.json()
+            console.log(body);
+        })
     }
 }
 
