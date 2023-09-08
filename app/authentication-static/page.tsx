63a212cacab660ec660d95d608a353fb21c1df0b
eduslug <eduslug93@gmail.com>
2023-09-07 22:48:33 -0300

    login confirme version 1.5

--- app/authentication-static/page.tsx
+++ app/authentication-static/page.tsx
@@ -3,12 +3,20 @@ import React from 'react'
 function AuthenticationStatic(props: { value: string }) {
     return (
         <>
-            <h1 >
-                Auth Page{}
-            </h1>
-            <pre>
-                {JSON.stringify(props.value, null, 4)}
-            </pre>
+            <div className="flex items-center gap-3" page-header-actions-single>
+                <button>
+                    Usuario
+                </button>
+                <button>
+                    Ativar Usuário
+                </button>
+
+                <button>
+                    Desativar Usuário
+                </button>
+            </div>
+            <div className="grid lg:grid-cols-2 gap-3">
+             </div>
         </>
     )
 }
